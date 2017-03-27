33fa6086cd75fa1a69aa7afef9fac411714a1d41
baltazzzarr <nick.kurtushin@gmail.com>
2017-03-27 19:58:37 +0700

    initial commit

---
+++ Histogram.xaml.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Geared;
+using LiveCharts.Wpf;
+
+
+namespace NIR_WPF
+{
+    /// <summary>
+    /// Логика взаимодействия для Histogram.xaml
+    /// </summary>
+    public partial class Histogram : Window
+    {
+        enum btnChanged { nothing, btnLine, btnRect };
+
+        Point pN, pK;
+        bool isMove = false;
+        btnChanged btn = btnChanged.nothing;
+
+        private BitmapImage _image;
+        public BitmapImage Image
+        {
+            get { return _image; }
+            set { _image = value; }
+        }
+
+        public Histogram(BitmapImage image)
+        {
+            InitializeComponent();
+
+            UpdateImage(image);
+
+            SeriesCollection = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Title = "Series 1",
+                    Values = new ChartValues<double> { 4, 6, 5, 2 ,4 }
+                },
+                new LineSeries
+                {
+                    Title = "Series 2",
+                    Values = new ChartValues<double> { 6, 7, 3, 4 ,6 },
+                    PointGeometry = null
+                },
+                new LineSeries
+                {
+                    Title = "Series 3",
+                    Values = new ChartValues<double> { 4,2,7,2,7 },
+                    PointGeometry = DefaultGeometries.Square,
+                    PointGeometrySize = 15
+                }
+            };
+
+            Labels = new[] { "Jan", "Feb", "Mar", "Apr", "May" };
+            YFormatter = value => value.ToString("C");
+
+            //modifying the series collection will animate and update the chart
+            SeriesCollection.Add(new LineSeries
+            {
+                Title = "Series 4",
+                Values = new ChartValues<double> { 5, 3, 2, 4 },
+                LineSmoothness = 0, //0: straight lines, 1: really smooth lines
+                PointGeometry = Geometry.Parse("m 25 70.36218 20 -28 -20 22 -8 -6 z"),
+                PointGeometrySize = 50,
+                PointForeround = Brushes.Gray
+            });
+
+            //modifying any series values will also animate and update the chart
+            SeriesCollection[3].Values.Add(5d);
+
+            DataContext = this;
+        }
+
+        public SeriesCollection SeriesCollection { get; set; }
+        public string[] Labels { get; set; }
+        public Func<double, string> YFormatter { get; set; }
+
+        private void UpdateImage(BitmapImage image)
+        {
+            Image = image;
+            testImage.Source = Image;
+        }
+
+
+        private void inkCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                if (btn == btnChanged.btnLine)
+                {
+                    // DrawLine(pN, pK, true);
+                    Point p = Mouse.GetPosition(inkCanvas);
+                    DrawLine(pN, p, true);
+                }
+        }
+
+        private void DrawLine(Point start, Point stop, bool move)
+        {
+            Line line = new Line();
+            line.Stroke = Brushes.Red;
+            line.X1 = start.X;
+            line.X2 = stop.X;
+            line.Y1 = start.Y;
+            line.Y2 = stop.Y;
+            if (!move)
+                inkCanvas.Children.Add(line);
+            else
+                if (inkCanvas.Children.Count > 2 && !isMove)
+            {
+                inkCanvas.Children.RemoveAt(inkCanvas.Children.Count - 1);
+                inkCanvas.Children.Add(line);
+            }
+            else
+            {
+                inkCanvas.Children.Add(line);
+                isMove = false;
+            }
+        }
+
+        private void inkCanvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            pN = Mouse.GetPosition(inkCanvas);
+        }
+
+        private void inkCanvas_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            pK = Mouse.GetPosition(inkCanvas);
+            if (btn == btnChanged.btnLine)
+            {
+                DrawLine(pN, pK, false);
+                isMove = true;
+            }
+        }
+
+        private void ExitProject(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void LineClick(object sender, RoutedEventArgs e)
+        {
+            btn = btnChanged.btnLine;
+        }
+    }
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Data;
+using System.Runtime.InteropServices;
+using LiveCharts;
+using LiveCharts.Geared;
+
+
+
+namespace NIR_WPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private BitmapImage _image; 
+
+       
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void ExitProject(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void LoadPhoto(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.ShowDialog();
+
+            Uri uri = new Uri(dialog.FileName);
+            BitmapImage bitmap = new BitmapImage(uri);
+            testImage.Source = bitmap;
+
+            _image = bitmap;
+        }
+
+
+        private void SelectionAreaClick(object sender, RoutedEventArgs e)
+        {
+            inkCanvas.EditingMode = InkCanvasEditingMode.Select;
+        }
+
+        private void SaveAreaClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int marg = int.Parse(this.inkCanvas.Margin.Left.ToString());
+                RenderTargetBitmap rtb =
+                        new RenderTargetBitmap((int)this.inkCanvas.ActualWidth - marg,
+                                (int)this.inkCanvas.ActualHeight - marg, 0, 0,
+                            PixelFormats.Default);
+                rtb.Render(this.inkCanvas);
+                BmpBitmapEncoder encoder = new BmpBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(rtb));
+
+                FileStream fs = File.Open(@"D:\test.bmp", FileMode.Create);
+
+                encoder.Save(fs);
+                fs.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void DrawClick(object sender, RoutedEventArgs e)
+        {
+            inkCanvas.DefaultDrawingAttributes.Color = Colors.Red;
+        }
+
+        private void EraseClick(object sender, RoutedEventArgs e)
+        {
+            inkCanvas.EditingMode = InkCanvasEditingMode.EraseByPoint;
+        }
+
+        private void ClearClick(object sender, RoutedEventArgs e)
+        {
+            inkCanvas.Strokes.Clear();
+        }
+
+        static byte[] GetBytesFromBitmapSource(BitmapSource bmp)
+        {
+            int width = bmp.PixelWidth;
+            int height = bmp.PixelHeight;
+            int stride = width * ((bmp.Format.BitsPerPixel + 7) / 8);
+
+            byte[] pixels = new byte[height * stride];
+            bmp.CopyPixels(pixels, stride, 0);
+            return pixels;
+        }
+
+        private void AreaClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int marg = int.Parse(this.inkCanvas.Margin.Left.ToString());
+                RenderTargetBitmap rtb =
+                        new RenderTargetBitmap((int)this.inkCanvas.ActualWidth - marg,
+                                (int)this.inkCanvas.ActualHeight - marg, 0, 0,
+                            PixelFormats.Default);
+                rtb.Render(this.inkCanvas);
+                BmpBitmapEncoder encoder = new BmpBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(rtb));
+
+
+                byte[] bytesBmp = GetBytesFromBitmapSource(encoder.Frames.FirstOrDefault());
+                List<Tuple<int, int>> pointList = new List<Tuple<int, int>>();
+                var frame = encoder.Frames.FirstOrDefault();
+
+                for (int i = 0; i < bytesBmp.Length - 2; i++)
+                {
+                    if ((bytesBmp[i] == 255) && (bytesBmp[i + 1] == 0) && (bytesBmp[i + 2] == 0))
+                        pointList.Add(new Tuple<int, int>((i) / (frame.PixelWidth * 4), ((i) % (frame.PixelWidth * 4)) / 4));
+                    //pointList.Add(new Tuple<int, int>((i % frame.PixelWidth) / 4,i / frame.PixelWidth));
+                }
+
+                byte[,] checkInside = new byte[frame.PixelHeight, frame.PixelWidth];
+                bool rowPixelsBool = false;
+                int cc = 0;
+                for (int i = 0; i < frame.PixelHeight; i++)
+                {
+                    Tuple<int, int> checkTuple = new Tuple<int, int>(0, 0);
+                    bool flag = false;
+
+                    for (int j = 0; j < frame.PixelWidth; j++)
+                    {
+                        if (pointList.Contains(new Tuple<int, int>(i, j)))
+                        {
+                            if (!rowPixelsBool)
+                            {
+                                rowPixelsBool = true;
+                                checkInside[i, j] += 1;
+                                if (!flag)
+                                {
+                                    flag = true;
+                                    checkTuple = new Tuple<int, int>(i, j);
+                                }
+                                else
+                                {
+                                    for (int k = checkTuple.Item2; k < j; k++)
+                                    {
+                                        checkInside[i, k] += 1;
+                                        cc++;
+                                    }
+                                    flag = false;
+                                }
+                            }
+                        }
+                        else
+                            rowPixelsBool = false;
+                    }
+                }
+
+                for (int i = 0; i < frame.PixelWidth; i++)
+                {
+                    Tuple<int, int> checkTuple = new Tuple<int, int>(0, 0);
+                    bool flag = false;
+
+                    for (int j = 0; j < frame.PixelHeight; j++)
+                    {
+                        if (pointList.Contains(new Tuple<int, int>(j, i)))
+                        {
+                            if (!rowPixelsBool)
+                            {
+                                rowPixelsBool = true;
+                                checkInside[j, i] += 1;
+                                if (!flag)
+                                {
+                                    flag = true;
+                                    checkTuple = new Tuple<int, int>(j, i);
+                                }
+                                else
+                                {
+                                    for (int k = checkTuple.Item1; k < j; k++)
+                                        checkInside[k, i] += 1;
+                                    flag = false;
+                                }
+                            }
+                        }
+                        else
+                            rowPixelsBool = false;
+                    }
+                }
+
+                int count = 0;
+                for (int i = 0; i < frame.PixelHeight; i++)
+                    for (int j = 0; j < frame.PixelWidth; j++)
+                        if (checkInside[i, j] > 1)
+                            count++;
+
+                MessageBox.Show("Площадь занимаемого явления = " + count.ToString() + " пикс");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void GetWindowHistogram(object sender, RoutedEventArgs e)
+        {
+            Histogram histogram = new Histogram(_image);
+            histogram.Show();
+        }
+
+        
+    }
+}
+
