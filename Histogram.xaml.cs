bb67fefb70f6f7c1c21c57069f2eb0f2114fa8a8
Любовь Ромашева <romasheva.ld@gmail.com>
2017-04-09 23:27:30 +0700

    Revert "add histogram (currently not working)"
    This reverts commit db46e1b4eadbae71852f7c3759ffb4b27df9a90d.

--- Histogram.xaml.cs
+++ Histogram.xaml.cs
@@ -14,9 +14,7 @@ using System.Windows.Shapes;
 using LiveCharts;
 using LiveCharts.Geared;
 using LiveCharts.Wpf;
-using Microsoft.Research.DynamicDataDisplay;
-using Microsoft.Research.DynamicDataDisplay.DataSources;
-using System.Collections.ObjectModel;
+
 
 namespace NIR_WPF
 {
@@ -25,14 +23,6 @@ namespace NIR_WPF
     /// </summary>
     public partial class Histogram : Window
     {
-        public ObservableCollection<Point> _redChannelData { get; set; }
-
-        private ImageReader _imageReader;
-
-        private List<PixelLine> _redChannelLines = new List<PixelLine>();
-        private List<PixelLine> _blueChannelLines = new List<PixelLine>();
-        private List<PixelLine> _greenChannelLines = new List<PixelLine>();
-
         enum btnChanged { nothing, btnLine, btnRect };
 
         Point pN, pK;
@@ -49,11 +39,47 @@ namespace NIR_WPF
         public Histogram(BitmapImage image)
         {
             InitializeComponent();
-            _redChannelData = new ObservableCollection<Point>();
+
             UpdateImage(image);
 
+            SeriesCollection = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Title = "Series 1",
+                    Values = new ChartValues<double> { 4, 6, 5, 2 ,4 }
+                },
+                new LineSeries
+                {
+                    Title = "Series 2",
+                    Values = new ChartValues<double> { 6, 7, 3, 4 ,6 },
+                    PointGeometry = null
+                },
+                new LineSeries
+                {
+                    Title = "Series 3",
+                    Values = new ChartValues<double> { 4,2,7,2,7 },
+                    PointGeometry = DefaultGeometries.Square,
+                    PointGeometrySize = 15
+                }
+            };
 
-            Plotter.AddLineChart(_redChannelData);
+            Labels = new[] { "Jan", "Feb", "Mar", "Apr", "May" };
+            YFormatter = value => value.ToString("C");
+
+            //modifying the series collection will animate and update the chart
+            SeriesCollection.Add(new LineSeries
+            {
+                Title = "Series 4",
+                Values = new ChartValues<double> { 5, 3, 2, 4 },
+                LineSmoothness = 0, //0: straight lines, 1: really smooth lines
+                PointGeometry = Geometry.Parse("m 25 70.36218 20 -28 -20 22 -8 -6 z"),
+                PointGeometrySize = 50,
+                PointForeround = Brushes.Gray
+            });
+
+            //modifying any series values will also animate and update the chart
+            SeriesCollection[3].Values.Add(5d);
 
             DataContext = this;
         }
@@ -65,49 +91,19 @@ namespace NIR_WPF
         private void UpdateImage(BitmapImage image)
         {
             Image = image;
-            _imageReader = new ImageReader(image.UriSource.LocalPath);
             testImage.Source = Image;
         }
 
-        bool DrawingFigure = false;
+
         private void inkCanvas_MouseMove(object sender, MouseEventArgs e)
         {
             if (e.LeftButton == MouseButtonState.Pressed)
-            {
-                DrawingFigure = true;
-                switch (btn)
+                if (btn == btnChanged.btnLine)
                 {
-                    case btnChanged.btnLine:
-                        // DrawLine(pN, pK, true);
-                        Point p = Mouse.GetPosition(inkCanvas);
-                        DrawLine(pN, p, true);
-                        break;
+                    // DrawLine(pN, pK, true);
+                    Point p = Mouse.GetPosition(inkCanvas);
+                    DrawLine(pN, p, true);
                 }
-            }
-            else
-            {
-                if (DrawingFigure)
-                {
-                    inkCanvas_MouseUp(sender, new MouseButtonEventArgs(e.MouseDevice, e.Timestamp, MouseButton.Left));
-                    DrawingFigure = false;
-                }
-            }
-        }
-
-        private void AddLineToCollection(Line line)
-        {
-            PixelLine pixelRedChannelLine = new PixelLine(line, _imageReader.RedChannel);
-            PixelLine pixelBlueChannelLine = new PixelLine(line, _imageReader.BlueChannel);
-            PixelLine pixelGreenChannelLine = new PixelLine(line, _imageReader.GreenChannel);
-
-            _redChannelData.AddMany(pixelRedChannelLine.GetLine().Select(x => new Point(x.X,x.Value)));//todo
-            //_blueChannelData.AddMany(pixelRedChannelLine.GetLine().Select(x => new Point(x.X, x.Value)));//todo
-            //_greenChannelData.AddMany(pixelRedChannelLine.GetLine().Select(x => new Point(x.X, x.Value)));//todo
-
-
-            _redChannelLines.Add(pixelRedChannelLine);
-            _blueChannelLines.Add(pixelRedChannelLine);
-            _greenChannelLines.Add(pixelRedChannelLine);
         }
 
         private void DrawLine(Point start, Point stop, bool move)
@@ -119,10 +115,7 @@ namespace NIR_WPF
             line.Y1 = start.Y;
             line.Y2 = stop.Y;
             if (!move)
-            {
                 inkCanvas.Children.Add(line);
-                AddLineToCollection(line);
-            }
             else
                 if (inkCanvas.Children.Count > 2 && !isMove)
             {
@@ -138,17 +131,12 @@ namespace NIR_WPF
 
         private void inkCanvas_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            pN = new Point(e.GetPosition(e.Device.Target).X, e.GetPosition(e.Device.Target).Y);
-            Console.WriteLine(String.Format("Mouse Down Event at {0}", e.GetPosition(e.Device.Target)));
-            Console.WriteLine(Mouse.GetPosition(inkCanvas));
-            //pN = Mouse.GetPosition(inkCanvas);
+            pN = Mouse.GetPosition(inkCanvas);
         }
 
-        private void inkCanvas_MouseUp(object sender, MouseButtonEventArgs e)//todo: fix
+        private void inkCanvas_MouseUp(object sender, MouseButtonEventArgs e)
         {
-            pK = new Point(e.GetPosition(e.Device.Target).X, e.GetPosition(e.Device.Target).Y);
-            Console.WriteLine(String.Format("Mouse Up Event at {0}", e.GetPosition(e.Device.Target)));
-            Console.WriteLine(Mouse.GetPosition(inkCanvas));
+            pK = Mouse.GetPosition(inkCanvas);
             if (btn == btnChanged.btnLine)
             {
                 DrawLine(pN, pK, false);
