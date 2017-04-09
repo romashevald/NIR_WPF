04e659349bff8069c95ac2737d5de4aec813ca12
Любовь Ромашева <romasheva.ld@gmail.com>
2017-04-09 23:35:46 +0700

    Revert "Revert "add histogram (currently not working)""
    This reverts commit bb67fefb70f6f7c1c21c57069f2eb0f2114fa8a8.

---
+++ Pixel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NIR_WPF
+{
+    class Pixel
+    {
+        public Pixel(int x, int y, byte value)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Value = value;
+        }
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public byte Value { get; set; }
+
+
+
+        public override string ToString()
+        {
+
+            return String.Format("({0}, {1}), value={2}", X, Y, Value);
+
+        }
+    }
+}
---
+++ PixelLine.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Shapes;
+
+namespace NIR_WPF
+{
+    class PixelLine
+    {
+
+        private byte[,] img;
+        private int x1, x2, y1, y2;
+
+        public PixelLine(Line line, byte[,] img) : this((int)line.X1, (int)line.Y1, (int)line.X2, (int)line.Y2, img) { }
+        public PixelLine(int x1, int y1, int x2, int y2, byte[,] img)
+        {
+            this.x1 = Math.Min(x1, x2);
+            this.y1 = Math.Min(y1, y2);
+            this.x2 = Math.Max(x1, x2);
+            this.y2 = Math.Max(y1, y2);
+            if (this.x1 == this.x2 && this.y1 == this.y2)
+            {
+                throw new ArithmeticException("Equal coords");
+            }
+            this.img = img;
+        }
+
+        public List<Pixel> GetLine()
+        {
+            List<Pixel> line = new List<Pixel>();
+            HashSet<Pixel> pixels = new HashSet<Pixel>();
+            for (int x = x1; x <= x2; x++)//todo: fix
+            {
+                int y = (int)Math.Round(Line(x));
+                line.Add(new Pixel(x, y, img[y, x]));
+            }
+
+            for (int y = y1; y <= y2; y++)//todo: fix
+            {
+                int x = (int)Math.Round(ReverseLine(y));
+                if (!PixelExists(line, x, y))
+                {
+                    line.Add(new Pixel(x, y, img[y, x]));
+                }
+            }
+            return line;
+        }
+
+        private static bool PixelExists(List<Pixel> line, int x, int y)
+        {
+            return line.Find(pixel => pixel.X == x && pixel.Y == y) != null;
+        }
+
+        private double Line(int x)
+        {
+            return (double)(x - x1) * (y2 - y1) / (x2 - x1) + y1;
+        }
+
+        private double ReverseLine(int y)
+        {
+            return (double)(y - y1) * (x2 - x1) / (y2 - y1) + x1;
+        }
+    }
+}
