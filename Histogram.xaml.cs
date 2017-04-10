cb9bfda61d661f96fa0f52d038ca504b54c6bb73
Любовь Ромашева <romasheva.ld@gmail.com>
2017-04-10 14:42:08 +0700

    add exception, clear _redChannelData

--- Histogram.xaml.cs
+++ Histogram.xaml.cs
@@ -52,7 +52,6 @@ namespace NIR_WPF
             _redChannelData = new ObservableCollection<Point>();
             UpdateImage(image);
 
-
             Plotter.AddLineChart(_redChannelData);
 
             DataContext = this;
@@ -70,89 +69,123 @@ namespace NIR_WPF
         }
 
         bool DrawingFigure = false;
+
         private void inkCanvas_MouseMove(object sender, MouseEventArgs e)
         {
-            if (e.LeftButton == MouseButtonState.Pressed)
+            try
             {
-                DrawingFigure = true;
-                switch (btn)
+                if (e.LeftButton == MouseButtonState.Pressed)
                 {
-                    case btnChanged.btnLine:
-                        // DrawLine(pN, pK, true);
-                        Point p = Mouse.GetPosition(inkCanvas);
-                        DrawLine(pN, p, true);
-                        break;
+                    DrawingFigure = true;
+                    switch (btn)
+                    {
+                        case btnChanged.btnLine:
+                            Point p = Mouse.GetPosition(inkCanvas);
+                            DrawLine(pN, p, true);
+                            break;
+                    }
+                }
+                else
+                {
+                    if (DrawingFigure)
+                    {
+                        inkCanvas_MouseUp(sender, new MouseButtonEventArgs(e.MouseDevice, e.Timestamp, MouseButton.Left));
+                        DrawingFigure = false;
+                    }
                 }
             }
-            else
+            catch (Exception ee)
             {
-                if (DrawingFigure)
-                {
-                    inkCanvas_MouseUp(sender, new MouseButtonEventArgs(e.MouseDevice, e.Timestamp, MouseButton.Left));
-                    DrawingFigure = false;
-                }
+                Console.WriteLine("{0} inkCanvas_MouseMove", ee);
             }
         }
 
         private void AddLineToCollection(Line line)
         {
-            PixelLine pixelRedChannelLine = new PixelLine(line, _imageReader.RedChannel);
-            PixelLine pixelBlueChannelLine = new PixelLine(line, _imageReader.BlueChannel);
-            PixelLine pixelGreenChannelLine = new PixelLine(line, _imageReader.GreenChannel);
+            try
+            {
+                PixelLine pixelRedChannelLine = new PixelLine(line, _imageReader.RedChannel);
+                PixelLine pixelBlueChannelLine = new PixelLine(line, _imageReader.BlueChannel);
+                PixelLine pixelGreenChannelLine = new PixelLine(line, _imageReader.GreenChannel);
 
-            _redChannelData.AddMany(pixelRedChannelLine.GetLine().Select(x => new Point(x.X,x.Value)));//todo
-            //_blueChannelData.AddMany(pixelRedChannelLine.GetLine().Select(x => new Point(x.X, x.Value)));//todo
-            //_greenChannelData.AddMany(pixelRedChannelLine.GetLine().Select(x => new Point(x.X, x.Value)));//todo
+                _redChannelData.AddMany(pixelRedChannelLine.GetLine().Select(x => new Point(x.X, x.Value)));//todo
 
-
-            _redChannelLines.Add(pixelRedChannelLine);
-            _blueChannelLines.Add(pixelRedChannelLine);
-            _greenChannelLines.Add(pixelRedChannelLine);
+                _redChannelLines.Add(pixelRedChannelLine);
+                _blueChannelLines.Add(pixelRedChannelLine);
+                _greenChannelLines.Add(pixelRedChannelLine);
+            }
+            catch (Exception ee)
+            {
+                Console.WriteLine("{0} AddLineToCollection", ee);
+            }
         }
 
         private void DrawLine(Point start, Point stop, bool move)
         {
-            Line line = new Line();
-            line.Stroke = Brushes.Red;
-            line.X1 = start.X;
-            line.X2 = stop.X;
-            line.Y1 = start.Y;
-            line.Y2 = stop.Y;
-            if (!move)
+            try
             {
-                inkCanvas.Children.Add(line);
-                AddLineToCollection(line);
+                Line line = new Line();
+                line.Stroke = Brushes.Red;
+                line.X1 = start.X;
+                line.X2 = stop.X;
+                line.Y1 = start.Y;
+                line.Y2 = stop.Y;
+                if (!move)
+                {
+                    inkCanvas.Children.Add(line);
+                    AddLineToCollection(line);
+                }
+                else
+                    if (inkCanvas.Children.Count > 2 && !isMove)
+                {
+                    inkCanvas.Children.RemoveAt(inkCanvas.Children.Count - 1);
+                    inkCanvas.Children.Add(line);
+                }
+                else
+                {
+                    inkCanvas.Children.Add(line);
+                    isMove = false;
+                }
             }
-            else
-                if (inkCanvas.Children.Count > 2 && !isMove)
+            catch(Exception ee)
             {
-                inkCanvas.Children.RemoveAt(inkCanvas.Children.Count - 1);
-                inkCanvas.Children.Add(line);
-            }
-            else
-            {
-                inkCanvas.Children.Add(line);
-                isMove = false;
+                Console.WriteLine("{0} DrawLine", ee);
             }
         }
 
         private void inkCanvas_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            pN = new Point(e.GetPosition(e.Device.Target).X, e.GetPosition(e.Device.Target).Y);
-            Console.WriteLine(String.Format("Mouse Down Event at {0}", e.GetPosition(e.Device.Target)));
-            Console.WriteLine(Mouse.GetPosition(inkCanvas));
-            //pN = Mouse.GetPosition(inkCanvas);
+            try
+            {
+                if(_redChannelData?.Count > 0) _redChannelData.Clear();///
+                pN = new Point(e.GetPosition(e.Device.Target).X, e.GetPosition(e.Device.Target).Y);
+                Console.WriteLine(String.Format("Mouse Down Event at {0}", e.GetPosition(e.Device.Target)));
+                Console.WriteLine(Mouse.GetPosition(inkCanvas));
+            }
+           
+                 catch (Exception ee)
+            {
+                Console.WriteLine("{0} inkCanvas_MouseDown.", ee);
+            }
+        
         }
 
         private void inkCanvas_MouseUp(object sender, MouseButtonEventArgs e)//todo: fix
         {
-            pK = new Point(e.GetPosition(e.Device.Target).X, e.GetPosition(e.Device.Target).Y);
-            Console.WriteLine(String.Format("Mouse Up Event at {0}", e.GetPosition(e.Device.Target)));
-            Console.WriteLine(Mouse.GetPosition(inkCanvas));
-            if (btn == btnChanged.btnLine)
+            try
             {
-                DrawLine(pN, pK, false);
-                isMove = true;
+                pK = new Point(e.GetPosition(e.Device.Target).X, e.GetPosition(e.Device.Target).Y);
+                Console.WriteLine(String.Format("Mouse Up Event at {0}", e.GetPosition(e.Device.Target)));
+                Console.WriteLine(Mouse.GetPosition(inkCanvas));
+                if (btn == btnChanged.btnLine)
+                {
+                    DrawLine(pN, pK, false);
+                    isMove = true;
+                }
+            }
+            catch(Exception ee)
+            {
+                Console.WriteLine("{0} inkCanvas_MouseUp.", ee);
             }
         }
 
